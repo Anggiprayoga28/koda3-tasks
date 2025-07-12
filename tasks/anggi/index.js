13ed707f619d6b1df492f4ad6f4734dcde139e7d
unknown <anggiprayoga930@gmail.com>
2025-07-12 16:23:29 +0700

    Feat: add new file index.js

---
+++ tasks/anggi/index.js
@@ -0,0 +1,15 @@
+const printSegitiga = require("./printSegitiga");
+const countArray = require("./countArray");
+const triangel = require("./triangle");
+const processNumbers = require("./processNumbers");
+const cetakTabel = require("./cetakTabel");
+const buatRentang = require("./buatRentang");
+
+module.exports = {
+    printSegitiga,
+    countArray,
+    triangel,
+    processNumbers,
+    cetakTabel,
+    buatRentang,
+};
