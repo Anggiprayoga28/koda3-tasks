ab018d8ad2b8178e93154a65eabc32ad1274336e
unknown <anggiprayoga930@gmail.com>
2025-07-12 14:52:36 +0700

    Feat: fix calling all functions in index.js

--- index.js
+++ index.js
@@ -45,7 +45,73 @@ function tasksIrul(){
 tasksIrul();
 
 /**
- * Task "Person Name"
+ * Task "Anggi Prayoga"
  */
 
 // import your tasks here
+const printSegitiga = require("./tasks/anggi/printSegitiga");
+const countArray = require ("./tasks/anggi/countArray");
+const triangle = require ("./tasks/anggi/triangle");
+const processNumbers = require ("./tasks/anggi/processNumbers");
+const cetakTabel = require ("./tasks/anggi/cetakTabel");
+const buatRentang = require ("./tasks/anggi/buatRentang");
+
+function taskAnggi (){
+    // 1. printSegitiga
+    try {
+        printSegitiga(1);
+
+        printSegitiga(2);
+
+        printSegitiga(5);
+     
+    } catch (error) {
+        console.log(error);
+    }
+
+    // 2. countArray
+    console.log(countArray([1, 2, 3, 4, 5]));
+    console.log(countArray([2, 2, 2, 2, 2]));
+
+    //3. triangel
+    try {
+                      
+    triangle("abc");
+
+    console.log('\ntriangle(-1)');
+    triangle(-1);
+
+    console.log('\ntriangle(1);');
+    triangle(1);
+
+    console.log('\ntriangle(2);');
+    triangle(2);
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    //4. processNumbers
+    console.log(processNumbers([1, 2, 3, 4, 5]));
+    console.log(processNumbers([2, 6, 4, 8]));
+
+
+    //5. cetakTabel
+    try {
+        
+        cetakTabel("abc");
+  
+        cetakTabel(-1);
+
+        cetakTabel(3);
+
+    } catch (error) {
+    console.log(error.message);
+    }
+
+    //6. buatRentang
+    buatRentang(1, 5);
+    buatRentang(5, 10);
+
+}
+
+taskAnggi();
